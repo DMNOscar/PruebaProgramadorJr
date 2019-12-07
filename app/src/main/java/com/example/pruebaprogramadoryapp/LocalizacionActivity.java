17130709905758811739a3f2e06f494242a0cb1d
OscarMartinez <31112744+DMNOscar@users.noreply.github.com>
2019-12-06 23:11:30 -0600

    GPS

---
+++ app/src/main/java/com/example/pruebaprogramadoryapp/LocalizacionActivity.java
@@ -0,0 +1,112 @@
+package com.example.pruebaprogramadoryapp;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.jar.Manifest;
+
+public class LocalizacionActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap googleMap;
+    private double latitud, longitud;
+
+    private LocationManager locationManager;
+    private Location location;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_localizacion);
+
+        Bundle bundle = getIntent().getExtras();
+        latitud = bundle.getDouble("LATITUD",0);
+        longitud = bundle.getDouble("LONGITUD",0);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        obtenerUbicacion(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+    }
+
+    public void obtenerUbicacion(Context context)
+    {
+        try
+        {
+            //CHECAMOS QUE LOS PERMISOS
+            if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            {
+                //ACTIVAMOS EL SERVICIO DE UBICACION DEL SISTEMA
+                locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+                //VARIABLES
+                boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+                if(isNetworkEnabled) {
+                    //PEDIMOS ACTUALICE LA UBICACION DEL SISTEMA
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,this );
+                    if(locationManager != null) //VERIFICAMOS QUE EL LOCATIONMANAGER SEA DIFERENTE DE NULL
+                    {
+                        //PEDIMOS LA ULTIMA LOCALIZACION OBTENIDA POR EL SISTEMA.
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        latitud = location.getLatitude();
+                        longitud = location.getLongitude();
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        latitud = location.getLatitude();
+        longitud = location.getLongitude();
+
+        LatLng ubicacionUsuario = new LatLng(latitud, longitud);
+        googleMap.addMarker(new MarkerOptions().position(ubicacionUsuario).title("Estas aqui"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacionUsuario,16));
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
--- app/src/main/java/com/example/pruebaprogramadoryapp/MainActivity.java
+++ app/src/main/java/com/example/pruebaprogramadoryapp/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.pruebaprogramadoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -12,15 +22,32 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FloatingActionButton fabActivityMapa;
+    LocationManager locationManager;
+    Location localizacion;
+    private double latitud=0, longitud=0;
+    private static final int REQUEST_CODE = 1;
+    private static final String PERMISOS[] = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         fabActivityMapa = findViewById(R.id.fabMapa);
 
+
         fabActivityMapa.setOnClickListener(this);
+
+
+/*        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISOS, REQUEST_CODE);
+                return;
+            }
+        }*/
+
+
 
     }
 
@@ -31,11 +58,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case  R.id.fabMapa:
 
-                Intent intent = new Intent(this, MapaActivity.class);
+                Toast.makeText(this, "Buscando Ubicacion", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, LocalizacionActivity.class);
                 startActivity(intent);
 
                 break;
 
         }
     }
+
+
+
+
 }
