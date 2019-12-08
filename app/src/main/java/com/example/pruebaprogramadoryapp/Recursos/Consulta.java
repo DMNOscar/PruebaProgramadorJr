f374aed82dfcf5ba20e043ceeeba7352ca5fa9b6
OscarMartinez <31112744+DMNOscar@users.noreply.github.com>
2019-12-07 21:33:38 -0600

    Depuracion

--- app/src/main/java/com/example/pruebaprogramadoryapp/Recursos/Consulta.java
+++ app/src/main/java/com/example/pruebaprogramadoryapp/Recursos/Consulta.java
@@ -6,18 +6,11 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-
-import javax.xml.transform.ErrorListener;
-
 public class Consulta extends AsyncTask<String, Void, String>{
 
     private static String URL_PETICION = "http://beta.yappapp.mx/test/json/apps";
